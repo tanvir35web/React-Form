4d68c483f95a0047e895f761225fcb623c4ec82f
tanvir35web <islammdtanvirul@gmail.com>
2024-01-17 21:05:18 +0600

    using formData method

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
+import SignUp from './components/Signup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        {/* <Login /> */}
+        <SignUp/>
       </main>
     </>
   );
